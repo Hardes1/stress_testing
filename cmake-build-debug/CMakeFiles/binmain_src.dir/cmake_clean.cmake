b7e86dd805c3633dd06df71177128d6fd0ff33ba
Hardes1 <ustinovgo@gmail.com>
2021-02-06 18:08:47 +0500

    my project

---
+++ cmake-build-debug/CMakeFiles/binmain_src.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binmain_src.dir/src/main.cpp.obj"
+  "binmain_src.exe"
+  "binmain_src.exe.manifest"
+  "binmain_src.pdb"
+  "libbinmain_src.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binmain_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/main_src.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_src.dir/src/main.cpp.obj"
+  "libmain_src.dll.a"
+  "main_src.exe"
+  "main_src.exe.manifest"
+  "main_src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
